b90af144a3e0485aa444c0c880110f115cb44375
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-14 12:11:46 +0530

    configure supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://wxywapntefztohssfexj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4eXdhcG50ZWZ6dG9oc3NmZXhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgzNDQ1NTcsImV4cCI6MjAzMzkyMDU1N30.rfG4f5e6yl3OOcFnIbbJkKlMC9TU43pbmiajJic8888";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;

7e2232acadf088bce4d5889e0924ffb1880e5c9c
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-15 11:07:45 +0530

    filter and sorting of bookings

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
-      staleTime: 0,
+      staleTime: 60 * 1000,
     },
   },
 });

4d1b09c9aa6348c26bfa539d073eed6004f1ae8f
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-16 16:30:27 +0530

    update user account

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -77,7 +77,12 @@ function SignupForm() {
       </FormRow>
 
       <FormRow>
-        <Button variation="secondary" type="reset" disabled={isLoading}>
+        <Button
+          variation="secondary"
+          type="reset"
+          disabled={isLoading}
+          onClick={reset}
+        >
           Cancel
         </Button>
         <Button disabled={isLoading}>Create new user</Button>

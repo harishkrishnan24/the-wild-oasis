faa979b1e3c6220cf778406857b421ba700d573e
Harish Krishnan <harishkrishnan1993@gmail.com>
2024-06-16 12:02:58 +0530

    login functionality

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,17 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
-import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
-      navigate("/dashboard");
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
-      console.error(err);
+      console.log("ERROR", err);
       toast.error("Provided email or password are incorrect");
     },
   });
